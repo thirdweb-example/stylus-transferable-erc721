40c8ab13d9957b9ea637517018fea19a37a81d24
Yash <kumaryashcse@gmail.com>
2025-11-09 21:26:59 +0530

    update struct s and function signatures

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use alloy_primitives::{Address, FixedBytes, U256};
 use alloy_sol_types::sol;
 use stylus_sdk::{
     abi::Bytes,
+    function_selector,
     host::VM,
     prelude::*,
     storage::{StorageBool, StorageMap},
@@ -18,31 +19,27 @@ const TRANSFERABLE_STORAGE_POSITION: U256 = U256::from_be_bytes([
     0x11, 0x68, 0x1d, 0x6b, 0x19, 0x8f, 0x7e, 0x7e, 0x1b, 0x6e, 0xf3, 0xeb, 0x45, 0x97, 0x6d, 0x00
 ]);
 
-pub const ERROR_TRANSFER_DISABLED: u8 = 1;
 
 sol! {
-    error TransferableError(uint8 code);
-}
+    #[derive(Debug, AbiType)]
+    struct CallbackFunction {
+        bytes4 selector;
+    }
 
-#[derive(SolidityError)]
-pub enum TransferableErrors {
-    TransferableError(TransferableError),
-}
+    #[derive(Debug, AbiType)]
+    struct FallbackFunction {
+        bytes4 selector;
+        uint256 permissionBits;
+    }
 
-pub struct CallbackFunction {
-    pub selector: FixedBytes<4>,
-}
-
-pub struct FallbackFunction {
-    pub selector: FixedBytes<4>,
-    pub permission_bits: U256,
-}
-
-pub struct ModuleConfig {
-    pub callback_functions: Vec<CallbackFunction>,
-    pub fallback_functions: Vec<FallbackFunction>,
-    pub required_interfaces: Vec<FixedBytes<4>>,
-    pub register_installation_callback: bool,
+    #[derive(Debug, AbiType)]
+    struct ModuleConfig {
+        bool registerInstallationCallback;
+        bytes4[] requiredInterfaces;
+        bytes4[] supportedInterfaces;
+        CallbackFunction[] callbackFunctions;
+        FallbackFunction[] fallbackFunctions;
+    }
 }
 
 struct TransferableStorage {
@@ -69,36 +66,51 @@ sol_storage! {
 
 #[public]
 impl StylusTransferableERC721 {
-
-    pub fn get_module_config(&self) -> (bool, Vec<FixedBytes<4>>, Vec<FixedBytes<4>>, Vec<FixedBytes<4>>, Vec<(FixedBytes<4>, U256)>) {
-        let register_installation_callback = false;
-        
-        let required_interfaces = vec![
-            FixedBytes::from([0x80, 0xac, 0x58, 0xcd]), // ERC721 interface
-        ];
-        
-        let supported_interfaces = vec![];
-        
-        let callback_functions = vec![
-            FixedBytes::from([0x31, 0x41, 0x59, 0x26]), // beforeTransferERC721 selector
-        ];
-        
-        let fallback_functions = vec![
-            (FixedBytes::from([0x13, 0x57, 0x9b, 0xd7]), U256::ZERO), // isTransferEnabled, no permission
-            (FixedBytes::from([0x29, 0x4c, 0x6f, 0x8a]), U256::ZERO), // isTransferEnabledFor, no permission
-            (FixedBytes::from([0x4a, 0x6e, 0x1b, 0x1c]), U256::from(2)), // setTransferable, _MANAGER_ROLE
-            (FixedBytes::from([0x5b, 0x7f, 0x2c, 0x3d]), U256::from(2)), // setTransferableFor, _MANAGER_ROLE
-        ];
-        
-        (register_installation_callback, required_interfaces, supported_interfaces, callback_functions, fallback_functions)
+    #[constructor]
+    pub fn constructor(&mut self) -> Result<(), String> {
+        Ok(())
     }
 
+    pub fn get_module_config(&self) -> Result<ModuleConfig, Vec<u8>> {
+        Ok(ModuleConfig {
+            registerInstallationCallback: false,
+            requiredInterfaces: vec![
+                FixedBytes::from([0x80, 0xac, 0x58, 0xcd]), // ERC721 interface
+            ],
+            supportedInterfaces: vec![],
+            callbackFunctions: vec![
+                CallbackFunction {
+                    selector: FixedBytes::from(function_selector!("beforeTransferERC721", Address, Address, U256)),
+                },
+            ],
+            fallbackFunctions: vec![
+                FallbackFunction {
+                    selector: FixedBytes::from(function_selector!("isTransferEnabled")),
+                    permissionBits: U256::ZERO,
+                },
+                FallbackFunction {
+                    selector: FixedBytes::from(function_selector!("isTransferEnabledFor", Address)),
+                    permissionBits: U256::ZERO,
+                },
+                FallbackFunction {
+                    selector: FixedBytes::from(function_selector!("setTransferable", bool)),
+                    permissionBits: U256::from(2), // _MANAGER_ROLE
+                },
+                FallbackFunction {
+                    selector: FixedBytes::from(function_selector!("setTransferableFor", Address, bool)),
+                    permissionBits: U256::from(2), // _MANAGER_ROLE
+                },
+            ],
+        })
+    }
+
+    #[selector(name = "beforeTransferERC721")]
     pub fn before_transfer_erc721(
         &mut self,
         from: Address,
         to: Address,
         _token_id: U256
-    ) -> Result<Bytes, TransferableErrors> {
+    ) -> Result<Bytes, String> {
         let storage = TransferableStorage::load(&self.vm());
         
         let is_operator_allowed = 
@@ -107,7 +119,7 @@ impl StylusTransferableERC721 {
             storage.transfer_enabled_for.get(to);
 
         if !is_operator_allowed && !storage.transfer_enabled.get() {
-            return Err(TransferableErrors::TransferableError(TransferableError { code: ERROR_TRANSFER_DISABLED }));
+            return Err("Transfer disabled".into());
         }
         
         Ok(Bytes(vec![].into()))
@@ -121,12 +133,12 @@ impl StylusTransferableERC721 {
         TransferableStorage::load(&self.vm()).transfer_enabled_for.get(target)
     }
 
-    pub fn set_transferable(&mut self, enable_transfer: bool) -> Result<(), TransferableErrors> {
+    pub fn set_transferable(&mut self, enable_transfer: bool) -> Result<(), String> {
         TransferableStorage::load(&self.vm()).transfer_enabled.set(enable_transfer);
         Ok(())
     }
 
-    pub fn set_transferable_for(&mut self, target: Address, enable_transfer: bool) -> Result<(), TransferableErrors> {
+    pub fn set_transferable_for(&mut self, target: Address, enable_transfer: bool) -> Result<(), String> {
         TransferableStorage::load(&self.vm()).transfer_enabled_for.insert(target, enable_transfer);
         Ok(())
     }
